2e88848715b30486b1cf9ded882d4296435346c8
remote-torrent-6747 <abbyc@sky.com>
2021-03-29 20:18:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -27,5 +28,5 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
-  
+
 end
