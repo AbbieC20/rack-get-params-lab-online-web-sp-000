154182160405dd07453bccadbb7e9ecea75558e6
remote-torrent-6747 <abbyc@sky.com>
2021-03-29 20:29:07 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,18 +17,7 @@ class Application
     else
       resp.write "Path Not Found"
     end
-
-    if req.path.match(/cart/)
-      @@cart.each do |cart_item|
-        resp.write "#{cart_item}\n"
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
-      resp.write "Your cart is empty"
-    end
-
+    
     resp.finish
   end
 
